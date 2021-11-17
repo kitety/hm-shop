70296ce2e53d9912aa932823d217fbad1e149ffe
念歌 <niange@qunhemail.com>
2021-11-17 14:51:38 +0800

    feat: reach bottom and pull down

--- pages/goods/goods.vue
+++ pages/goods/goods.vue
@@ -1,6 +1,7 @@
 <template>
-  <view>
+  <view class="goods">
     <goods-list :goods="goods"></goods-list>
+    <view v-if="noMoreData" class="bottom-line">----我是有底线的----</view>
   </view>
 </template>
 
@@ -10,32 +11,65 @@
   export default {
     data() {
       return {
-        pagenum: 1,
-        goods: []
+        pageNum: 1,
+        goods: [],
+        total: 0
       }
-    },
-    methods: {
-      // 获取商品数据
-      async getGoodsData() {
-        const data = {
-          pagenum: this.pagenum
-        }
-        const res = await this.$myRequest({
-          url: `/api/public/v1/goods/search?${qs.stringify(data)}`,
-        })
-        this.goods = res.data.message.goods;
-      },
-
     },
     onLoad() {
       this.getGoodsData()
     },
     components: {
       "goods-list": goodsList
+    },
+    computed: {
+      noMoreData: function() {
+        return this.goods.length >= this.total
+      }
+    },
+
+    methods: {
+      // 获取商品数据
+      async getGoodsData() {
+        const data = {
+          pagenum: this.pageNum
+        }
+        const res = await this.$myRequest({
+          url: `/api/public/v1/goods/search?${qs.stringify(data)}`,
+        })
+        this.goods = this.goods.concat(res.data.message.goods);
+        this.total = res.data.message.total
+        uni.stopPullDownRefresh()
+      },
+
+    },
+    // 触底更新
+    onReachBottom() {
+      // 如果还有数据
+      if (!this.noMoreData) {
+        this.pageNum++;
+        this.getGoodsData()
+      }
+    },
+    // 下拉刷新
+    onPullDownRefresh() {
+      // 初始化
+      this.goods = []
+      this.pageNum = 1
+      this.getGoodsData()
     }
+
   }
 </script>
 
-<style>
+<style lang="scss">
+  .goods {
+    background: #eee;
 
+    .bottom-line {
+      text-align: center;
+      margin-top: 8rpx;
+      color: #ccc;
+    }
+  }
 </style>
