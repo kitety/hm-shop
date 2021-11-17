af790c00bf905b5cb7990c4cb3675039fd5524f9
念歌 <niange@qunhemail.com>
2021-11-17 20:49:44 +0800

    feat: add map

--- pages/contact/contact.vue
+++ pages/contact/contact.vue
@@ -1,6 +1,12 @@
 <template>
-  <view>
-    
+  <view class="contact">
+    <image src="https://api-hmugo-web.itheima.net/pyg/banner1.png" class="image"></image>
+    <view class="info">
+      <view>联系电话：400-818-1233（点击拨打）</view>
+      <view>联系地址：中国四川省成都市青羊区骡马市天府广场</view>
+    </view>
+    <map :latitude="104.07231" :longitude="30.663467"></map>
+
   </view>
 </template>
 
@@ -8,15 +14,35 @@
   export default {
     data() {
       return {
-        
+
       }
     },
     methods: {
-      
+
     }
   }
 </script>
 
-<style>
+<style lang="scss">
+  .contact {
+    .image {
+      width: 750rpx;
+      height: 320rpx;
+    }
 
+    .info {
+      padding: 10rpx 20rpx;
+      font-size: 30rpx;
+
+      view {
+        line-height: 60rpx;
+        border-bottom: 1px solid #eee;
+      }
+    }
+
+    .map {
+      width: 750rpx;
+      height: 750rpx;
+    }
+  }
 </style>
